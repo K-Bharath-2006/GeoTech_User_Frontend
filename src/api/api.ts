3d23a22c29429cd750fcbbdc65d703501f00aff3
Bharath <gl.bharath2006@gmail.com>
2026-01-04 12:18:08 +0530

    Demo

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,6 @@ const BASE_URL = 'https://geotech-backend.onrender.com';
 export const API = {
   REGISTER: `${BASE_URL}/api/public/register`,
   LOGIN: `${BASE_URL}/api/public/login`,
-  ADDISSUE : `${BASE_URL}/api/issues/add`,
-   GET_MY_ISSUES: `${BASE_URL}/api/issues/my`,
+  ADDISSUE: `${BASE_URL}/api/issues/add`,
+  GET_MY_ISSUES: `${BASE_URL}/api/issues/my`,
 };
